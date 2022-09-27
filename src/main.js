639b649f2203e77269fc8ebdf332af47d36e578a
Javier Cruz Islas <javier.cruz.islas97@gmail.com>
2022-09-26 23:28:47 -0500

    Feature: Agregar Axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
+import AxiosPlugin from "vue-axios-cors";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-
+Vue.use(AxiosPlugin);
 new Vue({
   router,
   vuetify,
